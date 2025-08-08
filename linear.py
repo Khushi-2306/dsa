d5dd5a04155e304fb5d60adb987be1b56f3f7521
Khushi-2306 <khushishetty2307@gmail.com>
2025-08-08 15:09:49 +0530

    Add files via upload

---
+++ linear.py
@@ -0,0 +1,12 @@
+def linear_search(arr,target):
+    for i in range(len(arr)):
+        if arr[i]==target:
+            return i
+    return-1
+arr=[5,12,79,23,78,1]
+target=23
+result=linear_search(arr,target)
+if result!=-1:
+    print(f"element {target} found at index {result}")
+else:
+    print(f"element {target} not found in the list")
